a6ae292028a42fa203b8ce021e1305dbcf5b8a57
beanslee2012 <beanslee2007@gmail.com>
2020-11-25 09:29:41 +0800

    remake

--- splatoon2/jump.js
+++
@@ -1,6 +0,0 @@
-angular.module('splatApp').controller('jump', ['$scope', '$window', function ($scope, $window) {
-    $scope.redirectToGoogle = function () {
-    	 alter("xxx");
-        $window.open('https://www.google.com', '_blank');
-    };
-    });
